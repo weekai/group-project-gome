0464001a2dc171b1d30a77f8cea0578618f01ef2
FengJiu <934982309@qq.com>
2019-08-26 21:30:23 +0800

    lxr123

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    
+    <h2>lvxiaoran,zhenshuai!!!</h2>
     <router-view/>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+// 路由全局守卫函数
+router.beforeEach((to, from, next) => {
+  // console.log(to.meta.title)
+  document.title = to.meta.title
+  next()
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
