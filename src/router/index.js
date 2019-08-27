2496e7f54a494d49710de53471cd4f98d13e7a91
e <2437444862@qq.com>
2019-08-27 11:32:26 +0800

    在index.js中添加分类路由接口

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home/index.vue'
+import Category from '@/components/category/index.vue'
 
 Vue.use(Router)
 
@@ -15,6 +16,11 @@ export default new Router({
       path: '/home',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/category',
+      name: 'Category',
+      component: Category
     },
   ]
 })
